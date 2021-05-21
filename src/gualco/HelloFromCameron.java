fdf12226bb1f20da0032663a91a610b81079ae4a
Cgualco <cameron.gualco@gmail.com>
2021-05-20 20:33:20 -0600

    Prove 04 code

---
+++ src/gualco/HelloFromCameron.java
@@ -0,0 +1,9 @@
+package gualco;
+
+public class HelloFromCameron {
+
+    public static void main(String[] args) {
+        String name = "Cameron Gualco";
+        System.out.println("Hello, my name is " + name);
+    }
+}
