93bb232edc9e18942af6aa65c35e6530e226d6c3
IdahoBruin <calidaho951@gmail.com>
2021-05-22 23:03:51 -0600

    Update HelloFromCameron.java

--- src/gualco/HelloFromCameron.java
+++ src/gualco/HelloFromCameron.java
@@ -4,7 +4,8 @@ public class HelloFromCameron {
 
     public static void main(String[] args) {
         String name = "Cameron Gualco";
+        String teamMember = "Jacob Haney";
         System.out.println("Hello, my name is " + name);
-        System.out.println("Testing Undo Changes");
+        System.out.println("Hello Cameron, my name is " + teamMember);
     }
 }
